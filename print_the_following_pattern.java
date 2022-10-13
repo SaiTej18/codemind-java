21c39ce63d31b1b4d669b0fa6a4b38066bfb1715
SaiTej18 <112751550+SaiTej18@users.noreply.github.com>
2022-10-13 22:21:11 +0530

    import java.util.Scanner; class sai {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int n,k=0;         n=sc.nextInt();         for(int i=1;i<=n;i++)         {             for(int j=1;j<=n;j++)             {                 System.out.print((char)(64+i)+" ");             }             System.out.println();         }     } }

--- print_the_following_pattern.java
+++ print_the_following_pattern.java
@@ -4,18 +4,14 @@ class sai
     public static void main(String args[])
     {
         Scanner sc=new Scanner(System.in);
-        int r;
-        r=sc.nextInt();
-        for(int i=1;i<=2*r/2;i++)
+        int n,k=0;
+        n=sc.nextInt();
+        for(int i=1;i<=n;i++)
         {
-            int k=i>r?r-i%r:i;
-            for(int j=1;j<=r;j++)
+            for(int j=1;j<=n;j++)
             {
-                if(j<=r-k)
-                System.out.print(" ");
+                System.out.print((char)(64+i)+" ");
             }
-            for(int p=1;p<=2*k-1;p++)
-            System.out.print(i);
             System.out.println();
         }
     }
