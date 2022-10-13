5434719ccfdd81881e4623478f5a0c17277ca90a
SaiTej18 <112751550+SaiTej18@users.noreply.github.com>
2022-10-13 19:24:55 +0530

    import java.util.Scanner; class sai {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int n;         float sum=0;         n=sc.nextInt();         for(int i=1;i<=n;i++)         {             sum=sum+(float)(1.0f/i);         }         System.out.format("%.2f",sum);     } }

---
+++ Find_sum_of_series.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+class sai
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n;
+        float sum=0;
+        n=sc.nextInt();
+        for(int i=1;i<=n;i++)
+        {
+            sum=sum+(float)(1.0f/i);
+        }
+        System.out.format("%.2f",sum);
+    }
+}
